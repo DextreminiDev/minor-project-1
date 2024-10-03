56b52d4411925e820a2bfa88d5a0703ae5ad7519
Milind Sharma <milindsharma242@gmail.com>
2024-10-03 20:06:22 +0530

    Add test directory, update rle.cpp

---
+++ algorithms/rle.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void compress_text_file(const string& input_file, const string& output_file);
+void decompress_text_file(const string& input_file, const string& output_file);
+
+
+
+int main(void){
+    string input_txt = "";
+    string compressed_txt = "";
+    string decompressed_txt = "";
+
+    // Compress the text file
+    compress_text_file(input_txt, compressed_txt);
+    cout << "File compressed successfully." << endl;
+
+    // Decompress the text file
+    decompress_text_file(compressed_txt, decompressed_txt);
+    cout << "File decompressed successfully." << endl;
+
+    return 0;
+}
+
+
+
+// Function to compress a text file using RLE
+void compress_text_file(const string& input_file, const string& output_file){
+    ifstream in(input_file);
+    ofstream out(output_file);
+
+    if (!in || !out){
+        cerr << "Error opening file!" << endl;
+        return;
+    }
+
+    char current_char, previous_char;
+    int count = 1;
+
+    // Read the first character
+    in.get(previous_char);
+
+    // Iterate through the file
+    while (in.get(current_char)){
+        if (current_char == previous_char){
+            count++;
+        } else{
+            out << previous_char << count;  // Write character and count
+            previous_char = current_char;   // Update the current character
+            count = 1;
+        }
+    }
+
+    // Write the last character and count
+    out << previous_char << count;
+
+    in.close();
+    out.close();
+}
+
+
+
+// Function to decompress a compressed text file using RLE
+void decompress_text_file(const string& input_file, const string& output_file){
+    ifstream in(input_file);
+    ofstream out(output_file);
+
+    if (!in || !out){
+        cerr << "Error opening file!" << endl;
+        return;
+    }
+
+    char ch;
+    int count;
+
+    // Read character and count pair from input
+    while (in >> ch >> count){
+        out << string(count, ch);  // Write character 'count' times
+    }
+
+    in.close();
+    out.close();
+}
